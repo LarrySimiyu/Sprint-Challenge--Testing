f865a9cf28fc6dfe9586d5bc3de8beef10d5b561
LarrySimiyu <larry.simiyu@gmail.com>
2019-05-24 09:12:52 -0700

    MVP REACHED

---
+++ api/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'server is working' });
+});
+
+
+function logger(req, res, next) {
+    console.log(`${req.method} to ${req.url}`);
+    next();
+}
+
+server.use(logger);
+
+const games = [
+    {
+        title: 'Final Fantana',
+        genre: 'Booper',
+        releaseYear: 1973
+    },
+    {
+        title: 'NBA 2K19',
+        genre: 'Shooter',
+        releaseYear: 2019
+    },
+    {
+        title: 'Black Plague',
+        genre: 'Horror',
+        releaseYear: 1881
+    }
+];
+
+server.get('/games', (req, res) => {
+    res.status(200).json(games);
+})
+
+server.post('/games', (req, res) => {
+    let { title, genre, releaseYear } = req.body;
+    // check for required fields
+    if (!title || !genre) {
+        return res.status(422).json({ error: 'You must include a title and genre.' })
+    }
+
+    
+    return res.status(201).json({ message: `${title} added to games database.` })
+})
+
+module.exports = server;
